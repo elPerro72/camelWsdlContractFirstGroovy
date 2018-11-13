755c79ea8f264b8f5a993f622cb96f4047769df0
elPerro72 <raymonddehond@gmail.com>
2018-11-13 20:28:11 +0100

    initial version

---
+++ src/main/groovy/com/kiktan/camelWsdlFirstGroovy/CamelWsdlFirstGroovyApplication.groovy
@@ -0,0 +1,12 @@
+package com.kiktan.camelWsdlFirstGroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CamelWsdlFirstGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CamelWsdlFirstGroovyApplication, args
+	}
+}
